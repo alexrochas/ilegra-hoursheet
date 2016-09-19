5cf4b361956d8bbb055ce8c61dbf65f5715705c7
Alex Rocha <alex.rochas@yahoo.com.br>
2016-09-19 15:19:47 -0300

    refactor: get config objects

--- lancamento_horas/cron/cron.py
+++ lancamento_horas/cron/cron.py
@@ -1,12 +1,17 @@
 import sys
 import os
+import datetime
+
 path = os.path.dirname(os.path.abspath(__file__))
 path = os.path.dirname(os.path.dirname(path))
-sys.path.append(path) #the directory that contains my_pkg
+sys.path.append(path)
 
 from lancamento_horas import lancar_horas
 from lancamento_horas import console
-import datetime
+from configobj import ConfigObj
+
+
+config = ConfigObj("../../conf/perfil.config")
 
 
 def last_monday():
@@ -21,19 +26,22 @@ def morning_args():
 def afternoon_args():
     return '1210', '1920'
 
-args = console.parse_args()
-args.user = 'alex.silva'
-args.password = 'ilegra0712'
-args.yes = 'y'
-args.start, args.finish = morning_args()
-args.project = 1981
-args.subProject = 'SELECIONE...'
-args.type = 20
-args.description = '[place holder]'
-args.date = last_monday().strftime('%d/%m/%Y')
 
-lancar_horas.lancar(args)
+def run_cron():
+    args = console.parse_args()
+    args.user = config.get('usuario')
+    args.password = config.get('senha')
+    args.yes = 'n'
+    args.start, args.finish = morning_args()
+    args.project = 1981
+    args.subProject = 'SELECIONE...'
+    args.type = 20
+    args.description = '[place holder]'
+    args.date = last_monday().strftime('%d/%m/%Y')
+    lancar_horas.lancar(args)
+    args.start, args.finish = afternoon_args()
+    lancar_horas.lancar(args)
 
-args.start, args.finish = afternoon_args()
 
-lancar_horas.lancar(args)
+if __name__ == '__main__':
+    run_cron()
